e60da52cbbf26d309e17f3ab6358a96ac4eec3e8
Oscar Nunez <oscar.nunezcaba@gmail.com>
2023-03-21 16:28:58 -0400

    import multilogicchat from react chat

--- src/components/Chat/Index.jsx
+++ src/components/Chat/Index.jsx
@@ -1,6 +1,14 @@
 import React from 'react'
+import { useMultiChatLogic, MultiChatSocket, MultiChatWindow } from 'react-chat-engine-advanced'
 
 function Chat() {
+
+  const chatProps = useMultiChatLogic(
+    import.meta.env.VITE_PROJECT_ID,
+    "testuser",
+    "1234"
+  )
+
   return (
     <div>Chat</div>
   )
