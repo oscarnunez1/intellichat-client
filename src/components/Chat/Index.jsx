06b1fb8a4e19d6126f01c6d70e8bf55f01bf4555
Oscar Nunez <oscar.nunezcaba@gmail.com>
2023-03-22 16:28:37 -0400

    create CustomHeader component

--- src/components/Chat/Index.jsx
+++ src/components/Chat/Index.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useMultiChatLogic, MultiChatSocket, MultiChatWindow } from 'react-chat-engine-advanced'
-import Header from '@/components/Header'
+import Header from '@/components/CustomHeader'
 
 function Chat() {
   const chatProps = useMultiChatLogic(
---
+++ src/components/CustomHeader/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { ChatBubbleLeftRightIcon, PhoneIcon } from "@heroicons/react/24/solid"
+
+function CustomHeader() {
+  return (
+    <div>
+      <div>
+        
+      </div>
+    </div>
+  )
+}
+
+export default CustomHeader
