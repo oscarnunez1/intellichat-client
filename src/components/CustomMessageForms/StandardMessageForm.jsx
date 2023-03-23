3d357c04db50e8782b93f14bd85ebf40953a8205
Oscar Nunez <oscar.nunezcaba@gmail.com>
2023-03-22 20:01:11 -0400

    building out standard message form functionality

--- src/components/CustomMessageForms/StandardMessageForm.jsx
+++ src/components/CustomMessageForms/StandardMessageForm.jsx
@@ -1,8 +1,80 @@
-import React from 'react'
+import { PaperAirplaneIcon, PaperClipIcon, XMarkIcon } from '@heroicons/react/24/solid'
+import React, { useState } from 'react'
+import Dropzone from 'react-dropzone'
 
-const StandardMessageForm = () => {
+const StandardMessageForm = ({ props, activeChat }) => {
+  console.log('Active Chat', activeChat)
+  const [message, setMessage] = useState("")
+  const [attachement, setAttachment] = useState("")
+  const [preview, setPreview] = useState("")
+
+  const handleChange = (e) => setMessage(e.target.value)
+
+  const handleSubmit = async () => {
+
+  }
+
   return (
-    <div>StandardMessageForm</div>
+    <div className='message-form-container'>
+      {preview && (
+        <div className='message-form-preview'>
+          <img 
+            className='message-form-preview-image' 
+            src={preview} 
+            onLoad={() => URL.revokeObjectURL(preview)} 
+            alt='message-form-preview'
+          />
+          <XMarkIcon 
+            className='message-form-icon-x'
+            onClick={() => {
+              setPreview("")
+              setAttachment("")
+            }}
+          />
+        </div>
+      )}
+      <div className='message-form'>
+        <div className='message-form-input-container'>
+          <input
+            className='message-form-input'
+            type="text"
+            value={message}
+            onChange={handleChange}
+            placeholder='Send a messaage...'
+          />
+        </div>
+        <div className='message-form-icons'>
+          <Dropzone 
+            acceptedFiles='.jpg, .jpeg, .png'
+            multiple={false}
+            noClick={true}
+            onDrop={(acceptedFiles) => {
+              setAttachment(acceptedFiles[0])
+              setPreview(URL.createObjectURL(acceptedFiles[0]))
+            }}
+          >
+            {({ getRootProps, getInputProps, open}) => (
+              <div {...getRootProps()}>
+                <input {...getInputProps()} />
+                <PaperClipIcon 
+                  className='message-form-icon-clip'
+                  onClick={open}
+                />
+              </div>
+            )}
+          </Dropzone>
+
+          <hr className='vertical-line' />
+          <PaperAirplaneIcon 
+            className='message-form-icon-airplane'
+            onClick={() => {
+              setPreview("")
+              // handleSubmit()
+            }}
+          />
+        </div>
+      </div>
+    </div>
   )
 }
 
