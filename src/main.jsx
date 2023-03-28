a0ecc7791877a766a82cee3982c80288f5a10e5d
Oscar Nunez <oscar.nunezcaba@gmail.com>
2023-03-28 10:08:23 -0400

    move store variable to its own file for cleaner code in main

--- src/main.jsx
+++ src/main.jsx
@@ -2,5 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.scss'
+import { Provider } from 'react-redux'
+import { setupListeners } from '@reduxjs/toolkit/dist/query'
+import { store } from './state/store'
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />)
+setupListeners(store.dispatch)
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <Provider store={store}>
+      <App />
+  </Provider>
+)
