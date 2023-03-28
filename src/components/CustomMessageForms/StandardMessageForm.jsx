33199c93739621fc2c0d7d98fbfbcde629b8ba80
Oscar Nunez <oscar.nunezcaba@gmail.com>
2023-03-28 11:19:45 -0400

    pass in MessageFormUi component

--- src/components/CustomMessageForms/StandardMessageForm.jsx
+++ src/components/CustomMessageForms/StandardMessageForm.jsx
@@ -1,12 +1,9 @@
-import { PaperAirplaneIcon, PaperClipIcon, XMarkIcon } from '@heroicons/react/24/solid'
 import React, { useState } from 'react'
-import Dropzone from 'react-dropzone'
 import MessageFormUi from './MessageFormUi'
 
 const StandardMessageForm = ({ props, activeChat }) => {
   const [message, setMessage] = useState("")
   const [attachement, setAttachment] = useState("")
-  const [preview, setPreview] = useState("")
 
   const handleChange = (e) => setMessage(e.target.value)
 
@@ -30,66 +27,12 @@ const StandardMessageForm = ({ props, activeChat }) => {
   }
 
   return (
-    <div className='message-form-container'>
-      {preview && (
-        <div className='message-form-preview'>
-          <img 
-            className='message-form-preview-image' 
-            src={preview} 
-            onLoad={() => URL.revokeObjectURL(preview)} 
-            alt='message-form-preview'
-          />
-          <XMarkIcon 
-            className='message-form-icon-x'
-            onClick={() => {
-              setPreview("")
-              setAttachment("")
-            }}
-          />
-        </div>
-      )}
-      <div className='message-form'>
-        <div className='message-form-input-container'>
-          <input
-            className='message-form-input'
-            type="text"
-            value={message}
-            onChange={handleChange}
-            placeholder='Send a messaage...'
-          />
-        </div>
-        <div className='message-form-icons'>
-          <Dropzone 
-            acceptedFiles='.jpg, .jpeg, .png'
-            multiple={false}
-            noClick={true}
-            onDrop={(acceptedFiles) => {
-              setAttachment(acceptedFiles[0])
-              setPreview(URL.createObjectURL(acceptedFiles[0]))
-            }}
-          >
-            {({ getRootProps, getInputProps, open}) => (
-              <div {...getRootProps()}>
-                <input {...getInputProps()} />
-                <PaperClipIcon 
-                  className='message-form-icon-clip'
-                  onClick={open}
-                />
-              </div>
-            )}
-          </Dropzone>
-
-          <hr className='vertical-line' />
-          <PaperAirplaneIcon 
-            className='message-form-icon-airplane'
-            onClick={() => {
-              setPreview("")
-              handleSubmit()
-            }}
-          />
-        </div>
-      </div>
-    </div>
+    <MessageFormUi 
+      setAttachment={setAttachment}
+      message={message}
+      handleChange={handleChange}
+      handleSubmit={handleSubmit} 
+    />
   )
 }
 
